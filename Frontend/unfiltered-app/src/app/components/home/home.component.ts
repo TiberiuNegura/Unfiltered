46abad5ffa2dbbeb08ac8245ea4b2e1fbb03cd9b
Tiberiu Negura <neguratiberiucristian@gmail.com>
2024-12-14 18:13:04 +0200

    [backend][frontend] configured the backend and finished login + homepage frontend base

--- Frontend/unfiltered-app/src/app/components/home/home.component.ts
+++ Frontend/unfiltered-app/src/app/components/home/home.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    HeaderComponent,
-    FooterComponent
-  ],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
